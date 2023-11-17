1b32ee9410503b97890cd0525ebe90936ff746b8
Dogan <dogandagistanli@gmail.com>
2023-11-17 16:54:28 +0300

    Updated

--- c2311031.c
+++ c2311031.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 int main(){
+	int grade;
+	printf("Please enter your grade: ");
+	scanf("%d",&grade);
 	
-	
+if(grade>=0 && grade<=100){
+	if(grade>=90 && grade<=100){
+		printf("A");
+	if(grade>=75 && grade<=89){
+		printf("B");}
+	}
+	if(grade>=55 && grade<=74){
+		printf("C");}
+	}
+	if(grade>=45 && grade<=54){
+		printf("D");}
+	}
+	if(grade>=0 && grade<=44){
+		printf("F");}
+	}
+}else{
+	printf("Wrong input")
+}
 	
 	
 	
